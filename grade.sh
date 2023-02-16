7d96f81c089f9f234675053f7e14f21bd0fcddfa
AndyWeasley2004 <110661816+AndyWeasley2004@users.noreply.github.com>
2023-02-16 11:51:03 -0800

    Add files via upload

--- grade.sh
+++ grade.sh
@@ -1,8 +1,10 @@
+set -e
 CPATH='.;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar'
 
 rm -rf student-submission
 git clone $1 student-submission
 cd student-submission
+
 if [[ -f ListExamples.java ]]
     then 
         echo "ListExamples Found"
@@ -11,15 +13,22 @@ if [[ -f ListExamples.java ]]
         exit 1
 fi
 
+
+cp  ../TestListExamples.java ../student-submission
 javac -cp ".;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar" *.java
 
-if [[ -f ListExamples.class ]]
+
+javac TestListExamples.java
+
+if [[ $? -neq 0 ]]
     then 
         echo "ListExamples compiled"
     else
         echo "ListExamples failed to cmopile"
         exit 1
 fi
+
 echo "Finished compiling"
 
-java -cp ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore TestListExample
+set +e
+java -cp .;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore TestListExamples
